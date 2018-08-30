6dcf84906ebc22c5bf69a20c5f85824f5c598f0d
belger99 <belger99@yahoo.com>
2018-08-29 18:26:46 -0700

    Journal Update 2

---
+++ journal/cmake-build-debug/CMakeFiles/journal.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/journal.dir/main.cpp.obj"
+  "journal.pdb"
+  "journal.exe"
+  "journal.exe.manifest"
+  "libjournal.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/journal.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
